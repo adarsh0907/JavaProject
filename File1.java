c3bd22ecff6ed9b1893ba77a4432d7dfc89dc6c6
Patel <adarsh.a.a.patel@accenture.com>
2022-12-13 04:12:53 +0900

    count calculated

--- File1.java
+++ File1.java
@@ -1,9 +1,9 @@
 public class File1 {
     public static void main(String[] args) {
-        System.out.println("Hello worldddddd!");
+        int count=0;
         for(int i=0;i<10;i++){
-            System.out.println("Hii bye");
+            count+=i;
         }
-        System.out.println("bye worldddddd!");
+        System.out.println(count);
     }
 }
