5d33c4e66bbe4eab41e41b471d7cd03a031e145d
Patel <adarsh.a.a.patel@accenture.com>
2022-12-13 04:21:46 +0900

    new file created got calculating factorial of 9

--- File1.java
+++ File1.java
@@ -1,9 +1,9 @@
 public class File1 {
     public static void main(String[] args) {
-        int count=0;
+        int count1=0;
         for(int i=0;i<10;i++){
-            count+=i;
+            count1+=i;
         }
-        System.out.println(count);
+        System.out.println(count1);
     }
 }
